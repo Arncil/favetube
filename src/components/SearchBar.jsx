6d824f71a65c537f3e776321e89e2ba64ed0a419
Arncil <jackowiakmilosz@gmail.com>
2023-01-27 22:28:25 +0100

    Final Fixes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,7 +10,7 @@ const SearchBar = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         if (searchTerm) {
-            navigate(`/search/${searchTerm}`)
+            navigate(`/favetube/search/${searchTerm}`)
             setSearchTerm('')
         }
     }
